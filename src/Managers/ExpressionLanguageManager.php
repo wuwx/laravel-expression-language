c1defe46e434df8d6112ab4305fda7efd0b4d2c2
Weixin WU <wuweixin@gmail.com>
2018-01-29 20:41:51 +0800

    add array_get function

--- src/Managers/ExpressionLanguageManager.php
+++ src/Managers/ExpressionLanguageManager.php
@@ -2,6 +2,7 @@
 namespace Wuwx\LaravelExpressionLanguage\Managers;
 
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
 use Symfony\Component\ExpressionLanguage\SyntaxError;
 
 class ExpressionLanguageManager
@@ -11,6 +12,7 @@ class ExpressionLanguageManager
     public function __construct()
     {
         $this->expressionLanguage = new ExpressionLanguage();
+        $this->expressionLanguage->addFunction(ExpressionFunction::fromPhp('array_get'));
     }
 
     public function evaluate($expression, $values = array())
